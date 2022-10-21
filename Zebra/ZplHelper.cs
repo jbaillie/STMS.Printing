f67760101d78b528c80b4fa5781eea6d4b1fc9ea
jbaillie <34128045+jbaillie@users.noreply.github.com>
2022-10-21 08:53:09 +0200

    Add project files.

---
+++ Zebra/ZplHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STMS.Printing.Zebra
+{
+  public  class ZplHelper
+    {
+        #region "Print Settings"
+        public static string PrintSpeed { get; set; } = "2";
+        public static string Darkness { get; set; } = "0";
+        public static string MediaType { get; set; } = "M";
+        #endregion
+
+        private static string GetSetupFormat()
+        {
+            return "^XA" + Environment.NewLine +
+            "^PR" + PrintSpeed.ToString() + Environment.NewLine +
+            "^MD" + Convert.ToDouble(Darkness) + Environment.NewLine +
+            "^MNM";
+        }
+    }
+}
